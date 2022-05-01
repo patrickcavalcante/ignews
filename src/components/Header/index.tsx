3c9c6388d6c1f566a9d1a56325f1091f3bdaea94
patrickclima2 <patrickc.lima2@gmail.com>
2022-05-01 20:47:59 -0300

    feat/get post and list post and fix  header indicator

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink'
 
 export const Header: React.FC = () => {
   return (
@@ -8,10 +9,12 @@ export const Header: React.FC = () => {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo" />
         <nav>
-          <a href="" className={styles.active}>
-            Home
-          </a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/" >
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink  activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
